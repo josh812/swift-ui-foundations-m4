ab633187811692b472ddeb9b5ee325c3b7f25c2c
Josh812 <joshua.mccrory@icloud.com>
2022-01-29 20:26:15 -0800

    - Add recipe Featured View - Modified Data flow - Create Recipe Model instance in recipe tab view

---
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -0,0 +1,22 @@
+//
+//  RecipeFeaturedView.swift
+//  Recipe List App
+//
+//  Created by J M on 1/29/22.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    @EnvironmentObject var model:RecipeModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
--- Recipe List App/Views/RecipeTabView.swift
+++ Recipe List App/Views/RecipeTabView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct RecipeTabView: View {
     var body: some View {
         TabView {
-            Text("Featured View")
+            RecipeFeaturedView()
                 .tabItem {
                     VStack {
                         Image(systemName: "star.circle")
@@ -26,6 +26,7 @@ struct RecipeTabView: View {
                     }
                 }
         }
+        .environmentObject(RecipeModel())
     }
 }
 
